cb9881f81dce4f8c056dc595613e94342294958d
AkshitaMeena-Jharwal <akshita10298@gmail.com>
2024-09-02 11:29:14 +0000

    conversion of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "akshita10298@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2
+accountEmail = "hgshysah"
+accountPassword = "3568841"
+accountCity = "bangalore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword ,accountCity,accountState])
+/*
+Prefer not to use var bcoz of issue in block scope and functional scope
+*/
