30f73fd1c9c5bf8e72607e6c9a0b706798b00be4
AkshitaMeena-Jharwal <akshita10298@gmail.com>
2024-09-02 11:13:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") // we are using nodejs , not browser
+
+console.log(3+3)
+console.log("Akshita")
+
+let name = "Akshita"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
