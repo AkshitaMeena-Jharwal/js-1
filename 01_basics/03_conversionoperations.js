2a5e8651eddf92e53c220bccc2409a3b8f8f7ab4
AkshitaMeena-Jharwal <akshita10298@gmail.com>
2024-09-02 12:57:00 +0000

    conversion module

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+// let score = "33abc"
 
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -11,16 +11,22 @@ let score = "33abc"
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanisLoggedIn =  Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// let booleanisLoggedIn =  Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
 
 // 1=>true 0=>false
 // "" => false
 // "Akshita" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+//***************************OPERATIONS *//
+
+let value = 3
+let negValue = -value
+console.log();
